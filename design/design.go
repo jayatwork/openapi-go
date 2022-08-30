b3f957ffd3df5abea2adce29bb05d5555e200573
Jason Ancrum <jason.ancrum00@delta.com>
2022-08-30 08:17:06 -0400

    adding all initial files

---
+++ design/design.go
@@ -0,0 +1,55 @@
+package design
+
+import (
+	"fmt"
+
+	. "goa.design/goa/v3/dsl"
+)
+
+var _ = API("users", func() {
+	Title("users API")
+	Version("0.0.1")
+	Server("http", func() {
+		Host("development", func() {
+			URI("http://localhost:8080")
+		})
+	})
+})
+
+var User = Type("User", func() {
+	Attribute("name", func() {
+		MinLength(5)
+	})
+
+	Attribute("birth_year", func() {
+		Minimum("1900")
+		Maximum("2022")
+	})
+
+	Required("name", "birth_year")
+})
+
+var _ = Service("users", func() {
+	Description("API for users")
+
+	Method("create", func() {
+		Payload(User)
+		Result(User)
+		HTTP(func() {
+			POST("/users")
+		})
+	})
+
+	Method("all", func() {
+		Result(ArrayOf(User))
+		HTTP(func() {
+			GET("/users")
+		})
+	})
+
+	Files("/openapi3.json", "./gen/http/openapi3.json")
+})
+
+func main() {
+	fmt.Println("begin main block goa design startup")
+}
